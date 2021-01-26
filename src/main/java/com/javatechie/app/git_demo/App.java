228cb7ec7b8258ff03b513745af596b48124f7c3
Ahani123 <58323083+Ahani123@users.noreply.github.com>
2021-01-26 15:26:34 +0530

    added if condition to check the data

--- src/main/java/com/javatechie/app/git_demo/App.java
+++ src/main/java/com/javatechie/app/git_demo/App.java
@@ -10,6 +10,11 @@ public class App
 	public void addData(String data)
 	{
 		System.out.println("Data " +data);
+		
+		if(data.equals("java techie"))
+		{
+			System.out.println("welome");
+		}
 	}
 	
     public static void main( String[] args )
